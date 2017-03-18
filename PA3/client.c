0aaff8dc5bcb585c3263429ecd9f14601414065f
s7aesis <ryancd774@gmail.com>
2017-03-18 10:59:43 -0700

    Added call to initialize the verify server

--- PA3/client.c
+++ PA3/client.c
@@ -28,6 +28,29 @@ void InitAppendServer(char* hostname, AppendArgs args)
     clnt_destroy(clnt);
 }
 
+void InitVerifyServer(char* hostname, VerifyArgs args)
+{
+    CLIENT *clnt;
+    int  *result;
+
+    clnt = clnt_create(hostname, RPC_VerifyServer, RPC_VerifyServer_VERS, "udp");
+    if (clnt == NULL) {
+        clnt_pcreateerror(hostname);
+        exit(1);
+    }
+
+    result = rpc_initverifyserver_1(&args, clnt);
+
+    if (result == NULL) {
+        clnt_perror(clnt, hostname);
+    }
+    else {
+        printf("Result: %d\n", *result);
+    }
+
+    clnt_destroy(clnt);
+}
+
 int main(int argc, char **argv)
 {
     char *hostname1 = "localhost";
@@ -41,7 +64,13 @@ int main(int argc, char **argv)
     args.c1 = 'b';
     args.c2 = 'c';
 
+    VerifyArgs vArgs;
+    vArgs.numThreads = 3;
+    vArgs.segLength = 6;
+    vArgs.numSegments = 3;
+
     InitAppendServer(hostname1, args);
+    InitVerifyServer(hostname1, vArgs);
 
     return 0;
 }
