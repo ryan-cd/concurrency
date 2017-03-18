29b1eee4b53308b5ca4b523038a5627653f73b80
Don Pham <dpham@live.ca>
2017-03-18 15:02:36 -0400

    Added append string feature; Added a single-threaded test function for it.

--- PA3/appendserver_proc.c
+++ PA3/appendserver_proc.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
+#include <pthread.h>
 #include "appendserver.h"
 
+/* String */
+typedef struct {
+    char* str;
+    unsigned int length;
+    unsigned int index;
+} Str;
+
+/* Globals */
+Str string;
+
+/* RPC Procedure: int RPC_InitAppendServer(AppendArgs) */
 int *rpc_initappendserver_1_svc(AppendArgs *args, struct svc_req *req)
 {
-	static int result = 1;
+	static int result = 0;
 	printf("RPC_InitAppendServer\n");
+
+	// Init string
+	string.length = args->numSegments * args->segLength;
+	string.str = calloc(string.length + 1, sizeof(char));
+	string.index = 0;
 
 	return &result;
 }
 
+/* RPC Procedure: int RPC_Append(char) */
 int *rpc_append_1_svc(char *letter, struct svc_req *req)
 {
-	static int result = 1;
+	static int result = 0;
 	printf("RPC_Append\n");
+	printf("string: %s\n", string);
+
+	// Append letter if there is space
+	if (string.index < string.length) {
+		string.str[string.index++] = *letter;
+	} else {
+		result = -1;
+	}
 
 	return &result;
 }
--- PA3/client.c
+++ PA3/client.c
@@ -4,8 +4,7 @@
 #include "appendserver.h"
 #include "verifyserver.h"
 
-
-void InitAppendServer(char* hostname, AppendArgs args)
+void InitAppendServer(char *hostname, AppendArgs args)
 {
     CLIENT *clnt;
     int  *result;
@@ -28,7 +27,7 @@ void InitAppendServer(char* hostname, AppendArgs args)
     clnt_destroy(clnt);
 }
 
-void InitVerifyServer(char* hostname, VerifyArgs args)
+void InitVerifyServer(char *hostname, VerifyArgs args)
 {
     CLIENT *clnt;
     int  *result;
@@ -51,6 +50,41 @@ void InitVerifyServer(char* hostname, VerifyArgs args)
     clnt_destroy(clnt);
 }
 
+int Append(char *hostname, char letter)
+{
+    CLIENT *clnt;
+    int  *result;
+
+    clnt = clnt_create(hostname, RPC_AppendServer, RPC_AppendServer_VERS, "udp");
+    if (clnt == NULL) {
+        clnt_pcreateerror(hostname);
+        exit(1);
+    }
+
+    result = rpc_append_1(&letter, clnt);
+
+    if (result == NULL) {
+        clnt_perror(clnt, hostname);
+    }
+
+    clnt_destroy(clnt);
+
+    return *result;
+}
+
+void threadFunc(char *hostname, int thread)
+{
+    int letter = 'a' + thread;
+    int appendResult = 0;
+    while (appendResult == 0) {
+        // Sleep for a random period between 100ms and 500ms.
+        unsigned int microseconds = (rand() % (500000 + 1 - 100000)) + 100000; // Biased due to modulus.
+        usleep(microseconds);
+        // Append letter
+        appendResult = Append(hostname, letter);
+    }
+}
+
 int main(int argc, char **argv)
 {
     char *hostname1 = "localhost";
@@ -58,8 +92,8 @@ int main(int argc, char **argv)
 
     AppendArgs args;
 	args.property = 0;
-	args.segLength = 1;
-	args.numSegments = 1;
+	args.segLength = 4;
+	args.numSegments = 2;
 	args.c0 = 'a';
     args.c1 = 'b';
     args.c2 = 'c';
@@ -71,6 +105,9 @@ int main(int argc, char **argv)
 
     InitAppendServer(hostname1, args);
     InitVerifyServer(hostname1, vArgs);
+
+    // Testing
+    threadFunc(hostname1, 0);
 
     return 0;
 }
