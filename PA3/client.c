748f5dd5bd22a774452976cf70c2e395921a62be
Don Pham <dpham@live.ca>
2017-03-18 18:56:39 -0400

    Adding argument parsing from pa2.

--- PA3/client.c
+++ PA3/client.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <unistd.h> // usleep
 #include <omp.h> // openmp
 
@@ -44,19 +45,19 @@ void InitVerifyServer(char *hostname, VerifyArgs args)
     clnt_destroy(clnt);
 }
 
-void threadFunc(char *hostname, int thread)
+void threadFunc(char *hostname1, char *hostname2, int thread)
 {
     // Connect to RPC_AppendServer
-    CLIENT *appendClient = appendClient = clnt_create(hostname, RPC_AppendServer, RPC_AppendServer_VERS, "udp");
+    CLIENT *appendClient = appendClient = clnt_create(hostname1, RPC_AppendServer, RPC_AppendServer_VERS, "udp");
     if (appendClient == NULL) {
-        clnt_pcreateerror(hostname);
+        clnt_pcreateerror(hostname1);
         printf("Thread #%d: unable to connect to AppendServer!\n", thread);
         exit(1);
     }
     // Connect to RPC_VerifyServer
-    CLIENT *verifyClient = verifyClient = clnt_create(hostname, RPC_VerifyServer, RPC_VerifyServer_VERS, "udp");
+    CLIENT *verifyClient = verifyClient = clnt_create(hostname2, RPC_VerifyServer, RPC_VerifyServer_VERS, "udp");
     if (verifyClient == NULL) {
-        clnt_pcreateerror(hostname);
+        clnt_pcreateerror(hostname2);
         printf("Thread #%d: unable to connect to AppendServer!\n", thread);
         exit(1);
     }
@@ -88,34 +89,171 @@ void threadFunc(char *hostname, int thread)
     clnt_destroy(verifyClient);
 }
 
+void printInstructions() {
+    fprintf(stderr,
+        "\nUsage: ./client i N L M c_0 c_1 c_2 hostname_1 hostname_2\n"
+        "Parameters:\n"
+        "\t i:   (0<=i<=3) The index of the property Fi which each segment of S needs to satisfy.\n"
+        "\t N:   (3<=N<=8) The number of threads.\n"
+        "\t L:   (0 < L)   The length of each segment of S.\n"
+        "\t M:   (0 < M)   The number of segments in S to generate.\n"
+        "\t c_i: (0<=i<=2) The letters to be used in the property check.\n"
+        "\t hostname_1:    The hostname of the Append server.\n"
+        "\t hostname_2:    The hostname of the Verify server.\n"
+        "Example: ./client 0 3 6 3 a b c mills moore\n");
+}
+
 int main(int argc, char **argv)
 {
-    int numThreads = 4;
-    char *hostname1 = "localhost";
-    char *hostname2 = "verifyserver";
+    size_t property = 0; // i is the index of the property Fi which each segment of S needs to satisfy.
+    size_t numThreads = 0; // N is the number of threads.
+    size_t segLength = 0; // L is the length of each segment of S.
+    size_t numSegments = 0; // M is the number of segments in S to generate.
+    char c[3]; // c[i], i in {0, 1, 2}, are the letters to be used in the property check.
+    char *hostname1; // Hostname of the append server.
+    char *hostname2; // Hostname of the verify server.
 
-    AppendArgs args;
-	args.property = 0;
-	args.segLength = 4;
-	args.numSegments = 20;
-	args.c0 = 'a';
-    args.c1 = 'b';
-    args.c2 = 'c';
+    ////////////////////////////////////////////////////////////////////////////
+    if (argc < 7)
+    {
+        printInstructions();
+        exit(1);
+    }
+    else
+    {
+        // The user will see all of their mistakes before the program exits.
+        bool errorEncountered = false;
 
-    VerifyArgs vArgs;
-    vArgs.numThreads = numThreads;
-    vArgs.segLength = 6;
-    vArgs.numSegments = 3;
+        property = strtol(argv[1], NULL, 10);
+        if (property < 0 || property > 3) {
+            fprintf(stderr, "Error: i must be in {0,1,2,3}.\n");
+            errorEncountered = true;
+        }
 
-    InitAppendServer(hostname1, args);
-    InitVerifyServer(hostname1, vArgs);
+        numThreads = strtol(argv[2], NULL, 10);
+        if (numThreads < 3 || numThreads > 8) {
+            fprintf(stderr, "Error: N must be in the range [3, 8].\n");
+            errorEncountered = true;
+        }
+
+        segLength = strtol(argv[3], NULL, 10);
+        if (segLength <= 0) {
+            fprintf(stderr, "Error: L must be greater than 0.\n");
+            errorEncountered = true;
+        }
+
+        numSegments = strtol(argv[4], NULL, 10);
+        if (numSegments <= 0) {
+            fprintf(stderr, "Error: M must be greater than 0.\n");
+            errorEncountered = true;
+        }
+
+        c[0] = *argv[5];
+        c[1] = *argv[6];
+        c[2] = *argv[7];
+
+        for (int i = 0; i < sizeof(c); ++i) {
+            if (c[i] < 'a' || c[i] > 'a'+7) { // 'a'+7 is 'h'
+                fprintf(stderr, "Error: c_i must be between the letters %c and %c.\n", 'a', 'a'+7);
+                errorEncountered = true;
+            }
+        }
+
+        hostname1 = argv[8];
+        hostname2 = argv[9];
+
+        // Check M/N has no remainder.
+        if (numSegments % numThreads != 0) {
+            fprintf(stderr, "Error: M must be evenly divisible by N.\n");
+            errorEncountered = true;
+        }
+
+        // Check if segments of length L with alphabet size N can have all strings valid.
+        bool validStringPossible = false;
+        if (numThreads >= 4) {
+            // It's always possible to make a valid string with 4 or more letters.
+            validStringPossible = true;
+            goto segmentCheckEnd;
+        }
+        for (int c0 = 0; c0 <= segLength; c0++) {
+            for (int c1 = 0; c1 <= segLength; c1++) {
+                for (int c2 = 0; c2 <= segLength; c2++) {
+                    if (segLength != c0 + c1 + c2) {
+                        continue;
+                    }
+
+                    switch(property) {
+                        case 0:
+                            if (((c0 + c1 == c2) && (numThreads >= 3)))
+                            {
+                                validStringPossible = true;
+                                goto segmentCheckEnd;
+                            }
+                            break;
+                        case 1:
+                            if (((c0 + 2*c1 == c2) && (numThreads >= 3)))
+                            {
+                                validStringPossible = true;
+                                goto segmentCheckEnd;
+                            }
+                            break;
+                        case 2:
+                            if (((c0 * c1 == c2) && (numThreads >= 3)))
+                            {
+                                validStringPossible = true;
+                                goto segmentCheckEnd;
+                            }
+                            break;
+                        case 3:
+                            if (((c0 - c1 == c2) && (numThreads >= 3)))
+                            {
+                                validStringPossible = true;
+                                goto segmentCheckEnd;
+                            }
+                            break;
+                        default:
+                            printf("Invalid check\n");
+                            errorEncountered = true;
+                            break;
+                    }
+                }
+            }
+        }
+        segmentCheckEnd: ; // label to an empty statement
+
+        if (!validStringPossible) {
+            errorEncountered = true;
+            fprintf(stderr, "Error: A valid string is impossible to create with the given parameters.\n");
+        }
+
+        // Exit if any error was encountered.
+        if (errorEncountered) {
+            printInstructions();
+            exit(1);
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////
+
+    AppendArgs appendArgs;
+    appendArgs.property = property;
+    appendArgs.segLength = segLength;
+    appendArgs.numSegments = numSegments;
+    appendArgs.c0 = c[0];
+    appendArgs.c1 = c[1];
+    appendArgs.c2 = c[2];
+
+    VerifyArgs verifyArgs;
+    verifyArgs.numThreads = numThreads;
+    verifyArgs.segLength = segLength;
+    verifyArgs.numSegments = numSegments;
+
+    InitAppendServer(hostname1, appendArgs);
+    InitVerifyServer(hostname1, verifyArgs);
 
     #pragma omp parallel for num_threads(numThreads)
     for (int i = 0; i < numThreads; ++i) {
-        threadFunc(hostname1, i);
+        threadFunc(hostname1, hostname2, i);
     }
-
 
     return 0;
 }
-
